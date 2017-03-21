5a99f72b066d236058e3d1ca9e029d74650fa3f9
jameshamann <jameshamann@me.com>
2017-03-21 15:29:06 +0000

    it finally works after a stupid typo

--- _plugins/medium_posts.rb
+++ _plugins/medium_posts.rb
@@ -13,25 +13,35 @@ module Jekyll
 
       Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@tomrobinson_33099").entries.each do |e|
         p "Title: #{e.title}, published on Medium #{e.url} #{e}"
-        title = e.title
-        content = e.content
-        guid = e.url
+        title = e[:title]
+        content = e[:content]
+        guid = e[:url]
         puts "---------JUNK PRINTING----------"
         puts "#{title}, #{content}, #{guid}"
-        path = "_rss/" + guid + ".md"
+        path = "./_external_feed/" + title + ".md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        puts "#{doc}"
-        doc.data['title'] = title
-        doc.data['feed_content'] = content
+        puts "---------JUNK PRINTING AGAIN----------"
+        puts "JEKYLCOLL----#{jekyll_coll}"
+        puts "#{site.collections['external_feed']}"
+        puts "#{doc.collection}"
+        puts "DOCDATA--------_!!!!!!"
+        doc.data['title'] = title;
+        doc.data['feed_content'] = content;
+        puts "DOC----#{doc.data}"
+        puts "HEEEELLLOOOOOO"
+        puts "#{jekyll_coll.docs}"
         jekyll_coll.docs << doc
+        puts "GOODBYEEEEEEE"
+        puts "#{jekyll_coll.docs}"
         puts "DOC----#{doc}"
         puts "JEKYLCOLL----#{jekyll_coll}"
-        puts "DOCDATA---#{doc.data}"
+        puts "#{jekyll_coll.docs}"
       end
 
       puts "----------SITE COLLECTION THING-------"
-      puts "#{jekyll_coll.docs[0]}"
+      puts "#{site.collections}"
+      puts "#{site.collections['external_feed'].docs}"
 
     end
   end
