bc3514f70c6678047ba7469694c8963a71792624
jameshamann <jameshamann@me.com>
2017-03-21 15:43:46 +0000

    refactors medium_post plugin

--- _plugins/medium_posts.rb
+++ _plugins/medium_posts.rb
@@ -1,6 +1,4 @@
 require 'feedjira'
-require 'kramdown'
-require 'uri'
 
 module Jekyll
   class MediumPostDisplay < Generator
@@ -16,33 +14,13 @@ module Jekyll
         title = e[:title]
         content = e[:content]
         guid = e[:url]
-        puts "---------JUNK PRINTING----------"
-        puts "#{title}, #{content}, #{guid}"
         path = "./_external_feed/" + title + ".md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        puts "---------JUNK PRINTING AGAIN----------"
-        puts "JEKYLCOLL----#{jekyll_coll}"
-        puts "#{site.collections['external_feed']}"
-        puts "#{doc.collection}"
-        puts "DOCDATA--------_!!!!!!"
         doc.data['title'] = title;
         doc.data['feed_content'] = content;
-        puts "DOC----#{doc.data}"
-        puts "HEEEELLLOOOOOO"
-        puts "#{jekyll_coll.docs}"
         jekyll_coll.docs << doc
-        puts "GOODBYEEEEEEE"
-        puts "#{jekyll_coll.docs}"
-        puts "DOC----#{doc}"
-        puts "JEKYLCOLL----#{jekyll_coll}"
-        puts "#{jekyll_coll.docs}"
       end
-
-      puts "----------SITE COLLECTION THING-------"
-      puts "#{site.collections}"
-      puts "#{site.collections['external_feed'].docs}"
-
     end
   end
 end
