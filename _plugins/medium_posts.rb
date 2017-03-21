174837d64f9bae929f7cf72b61c980a0de0f4280
jameshamann <jameshamann@me.com>
2017-03-21 13:20:15 +0000

    debugging to get vars saved, so many puts statements

--- _plugins/medium_posts.rb
+++ _plugins/medium_posts.rb
@@ -7,7 +7,6 @@ module Jekyll
     safe true
     priority :high
 
-
     def generate(site)
       jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
       site.collections['external_feed'] = jekyll_coll
@@ -16,13 +15,24 @@ module Jekyll
         p "Title: #{e.title}, published on Medium #{e.url} #{e}"
         title = e.title
         content = e.content
-        path = "_rss/" + ".md"
+        guid = e.url
+        puts "---------JUNK PRINTING----------"
+        puts "#{title}, #{content}, #{guid}"
+        path = "_rss/" + guid + ".md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        doc.data['title'] = title;
-        doc.data['feed_content'] = content;
+        puts "#{doc}"
+        doc.data['title'] = title
+        doc.data['feed_content'] = content
         jekyll_coll.docs << doc
+        puts "DOC----#{doc}"
+        puts "JEKYLCOLL----#{jekyll_coll}"
+        puts "DOCDATA---#{doc.data}"
       end
+
+      puts "----------SITE COLLECTION THING-------"
+      puts "#{jekyll_coll.docs[0]}"
+
     end
   end
 end
