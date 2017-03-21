d5dcdd3231b062a52d8189a950f7b683d2c4462e
jameshamann <jameshamann@me.com>
2017-03-21 12:23:07 +0000

    adds plugin for MediumPostDisplay, gathers information from rss feed, parses and saves in a collection

--- _plugins/medium_posts.rb
+++ _plugins/medium_posts.rb
@@ -1,10 +1,28 @@
 require 'feedjira'
+require 'kramdown'
+require 'uri'
 
-def rss_parser(url)
-  Feedjira::Feed.fetch_and_parse(url).entries.each do |e|
-    p "Title: #{e.title}, published on Medium #{e.url}"
+module Jekyll
+  class MediumPostDisplay < Generator
+    safe true
+    priority :high
+
+
+    def generate(site)
+      jekyll_coll = Jekyll::Collection.new(site, 'external_feed')
+      site.collections['external_feed'] = jekyll_coll
+
+      Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@tomrobinson_33099").entries.each do |e|
+        p "Title: #{e.title}, published on Medium #{e.url} #{e}"
+        title = e.title
+        content = e.content
+        path = "_rss/" + ".md"
+        path = site.in_source_dir(path)
+        doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
+        doc.data['title'] = title;
+        doc.data['feed_content'] = content;
+        jekyll_coll.docs << doc
+      end
+    end
   end
 end
-
-
-rss_parser("https://medium.com/feed/@tomrobinson_33099")
